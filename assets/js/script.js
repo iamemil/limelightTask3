485a7c2e40148fdbca678fa6e839c114ca40b78d
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2021-05-07 01:16:19 +0200

    jumbotron done

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+   // Can also be included with a regular script tag
+   var typed = new Typed('.typed', {
+    strings: ["ASP.NET Developer<span style='color:#00ff88'>.</span>", "Web Developer<span style='color:#00ff88'>.</span>", "C# Developer<span style='color:#00ff88'>.</span>"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 2000,
+    loop:true
+  });
+});
